fbfab94d91ba6da534beed1a41e0c45305f24e3b
Kenneth Edward Tan <kenneth_edward_tan@dlsu.edu.ph>
2022-07-05 19:02:30 +0800

    added colorizer files, images

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from colorizers.siggraph17 import siggraph17
 from server import colorize
+from PIL import Image
+from werkzeug.utils import secure_filename
+import base64
+import urllib.request
+import os
 
 app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template('index.html')
