3854a768c4b92493e22d102068a0b4d168daa980
Kenneth Edward Tan <kenneth_edward_tan@dlsu.edu.ph>
2022-07-06 12:50:43 +0800

    app.py upload photo

--- app.py
+++ app.py
@@ -8,9 +8,30 @@ import urllib.request
 import os
 
 app = Flask(__name__)
+PHOTO_EXTENSION = set(['png', 'jpg', 'jpeg'])
+def allowed_file(before):
+    return '.' in before and before.rsplit('.', 1)[1].lower() in PHOTO_EXTENSION
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-#@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
+def upload_photo():
+    if file.filename == "":
+        flash("Please upload a photo.")
+        return redirect(request.url)
+    file = request.files["file"]
+    if allowed_file(file.filename) and file:
+        before = secure_filename(file.filename)
+        path = os.path.join(app.config["static/images/"], before)
+        file.save(path)
+        flash("Photo is now ready to be colorized.")
+        return render_template("index.html", filename=before, base64=colorize(path))
+    else:
+        flash("Please only upload png or jpg files.")
+        return redirect(request.url)
+
+if __name__ == "___main__":
+    app.run(debug=True)
+    app.debug=True
--- server.py
+++ server.py
@@ -5,36 +5,21 @@ import base64
 import matplotlib.pyplot as plt
 from colorizers import *
 
-# urls = ("/favicon.ico", "dummy")
-# app = Flask(__name__)
-
-# @app.route('/', methods=['GET'])
-# # def hello():
-#     return 1
-
-# @app.route('/post', methods=['POST'])
 def colorize(path="", use_gpu=False):
-    #get the input image here
-
-    # load model
+    # load siggraph17 model
     colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-    #if (use_gpu):
-	#    colorizer_siggraph17.cuda()
         
-    # store input image here
+    # store and process input image
     img = load_img(path)
     (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
     out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-    #save image here
-    #plt.imsave('result.png', out_img_siggraph17)
+    # save image 
     img_L = Image.fromarray((out_img_siggraph17*255).astype(np.uint8))
     img_B = BytesIO()
-    img_L.save(img_B, 'PNG')
+    img_L.save(img_B, "png")
     img_B.seek(0)
     img_F = base64.b64encode(img_B.read()).decode()
 
-    # return the image here
+    # return image
     return img_F
-#if __name__ == '__main__':
-#    app.run()
